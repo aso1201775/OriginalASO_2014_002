783def41e4cf35350618abc9473df15331665574
student <student@PC111S09.fgakusei.local>
2014-06-06 10:12:21 +0900

    おｋ

--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -3,14 +3,38 @@ package jp.ac.st.asojuku.original2014002;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		Button btn1 = (Button)findViewById(R.id.btn1);
+		btn1.setOnClickListener(this);
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
