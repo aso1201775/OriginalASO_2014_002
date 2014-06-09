cc610e8d0998f2f6396c5d47b2a21e41b65faf97
student <student@PC633S26.fgakusei.local>
2014-06-09 16:28:28 +0900

    遷移したよ

--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,10 +1,12 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements
 View.OnClickListener {
@@ -18,7 +20,25 @@ View.OnClickListener {
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
+		Intent vIntent = null;
+		switch(v.getId()){
+		case R.id.btn1:
+			vIntent = new Intent(this,MaintenanceActivity.class);
+			startActivity(vIntent);
+			break;
 
+		case R.id.btn2:
+			//エディットテキストから入力内容を取り出す
+			EditText etv = (EditText)findViewById(R.id.edtText);
+			String inputMsg = etv.getText().toString();
+			break;
+
+		case R.id.btn3:
+			//エディットテキストから入力内容を取り出す
+			vIntent = new Intent(this,HitokotoActivity.class);
+			startActivity(vIntent);
+			break;
+			}
 	}
 
 	@Override
@@ -33,6 +53,12 @@ View.OnClickListener {
 		super.onResume();
 		Button btn1 = (Button)findViewById(R.id.btn1);
 		btn1.setOnClickListener(this);
+
+		Button btn2 = (Button)findViewById(R.id.btn2);
+		btn2.setOnClickListener(this);
+
+		Button btn3 = (Button)findViewById(R.id.btn3);
+		btn3.setOnClickListener(this);
 	}
 
 
