b6e0313892ab417484f2d29b7737f84b9e626621
student <student@PC111S09.fgakusei.local>
2014-06-20 10:17:50 +0900

    かーーーーーんせーーーーーーーーい(`・ω・)

--- src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MaintenanceActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener{
 
@@ -56,13 +57,51 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
+		//前に選択中の行があれば、背景色を透明にする
+		if(this.selectdID!=1){
+			parent.getChildAt(this.lastPosition).setBackgroundColor(0);
+		}
+		//選択中の行の背景色をグレーにする
+		view.setBackgroundColor(android.graphics.Color.LTGRAY);
+
+		//選択行のレコードを指し示すカーソルを取得
+		SQLiteCursor cursor = (SQLiteCursor)parent.getItemAtPosition(position);
+		//カーソルのレコードから、[_id]の値を取得して記憶
+		this.selectdID = cursor.getInt(cursor.getColumnIndex("_id"));
+		//何行目を選択したかも記憶
+		this.lastPosition = position;
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
+		switch(v.getId()){ //どのボタンが押されたか判定
+			case R.id.button2:
+
+				//選択行があれば
+				if(this.selectdID !=1){
+					this.deleteFromHitokoto(this.selectdID);
+					ListView lstHitokoto = (ListView)findViewById(R.id.listView1);
+					//ListViewにDBをセット
+					this.setDBvaluetoList(lstHitokoto);
+					//選択行を忘れる
+					this.selectdID = -1;
+					this.lastPosition = -1;
+				}
+				else{
+					//なければ、トースト(簡易メッセージ)を表示
+					Toast.makeText(MaintenanceActivity.this, "削除する行を選んでください", Toast.LENGTH_SHORT).show();
+
+				}
+				break;
+
+			case R.id.button1:  //戻るボタンが押された
+				//今の画面Activityを消して、前の画面Activityに戻る
+				finish();
+				break;
+		}
 
 	}
 
@@ -96,6 +135,27 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
 
 		lstHitokoto.setAdapter(adapter);
+	}
+
+
+	/**
+	 * Hitokotoテーブルから、引数で指定した｢_id｣と同じ値を持つレコードを削除
+	 * @param id 指定する値
+	 */
+
+	private void deleteFromHitokoto(int id){
+		//クラスのフィールド変数がNULLなら、データベース空間オープン
+		if(sdb == null){
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			//異常終了
+			Log.e("ERROR", e.toString());
+		}
+		//MySQLiteOpenHelperにDELETE文を実行させる
+		this.helper.deleteHitokoto(sdb, id);
 	}
 
 }
